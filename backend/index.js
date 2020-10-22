2f19da563e059a42f6cd11687e3e617223c2efb3
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-22 14:49:52 -0300

    backend finalizado

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const server = express();
+
+
+mongoose.connect('mongodb+srv://softwrap:softwrap@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('foi carai')
+}).catch((err) => {
+    console.log('deu pau' + err);
+})
+server.use(express.json())
+server.use(bodyParser.json())
+server.use('/api', require('./routes'));
+  
+server.use(cors())
+
+server.listen(3000);
---
+++ backend/src/models/Person.js
@@ -0,0 +1,31 @@
+const { Schema, model} = require('mongoose')
+
+
+const PersonSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    civil_status: {
+        type: String,
+        required: true,
+    },
+    cpf: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = model('Person', PersonSchema)
