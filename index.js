bbeefa0ca451f2ae9bffa35f21d22bd3555e93d1
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-24 15:31:27 -0300

    testando

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const env = require("dotenv").config(".env");
+const env = require("dotenv").config({ path: "./env" });
 const server = express();
 
 mongoose
