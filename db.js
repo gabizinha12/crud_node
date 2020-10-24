a978a68ceaf87d5f4130e4d26608596ebe6d58cc
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-24 16:36:27 -0300

    teste com ternário

--- db.js
+++ db.js
@@ -1,11 +1,6 @@
-if (process.env.NODE_ENV == "production") {
-  module.exports = {
-    MONGODB_URI:
-      "mongodb+srv://deploy:deploy@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority",
-  };
-} else {
-  module.exports = {
-    MONGODB_URI:
-      "mongodb+srv://softwrap:softwrap@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority",
-  };
-}
+module.exports = {
+  MONGODB_URI:
+    NODE_ENV === "production"
+      ? "mongodb+srv://deploy:deploy@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority"
+      : "mongodb+srv://softwrap:softwrap@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority",
+};
