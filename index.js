dbfe334c40383485ee7d2d67649fe2b4367e30f7
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-24 20:40:06 -0300

    teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const env = require("dotenv").config({ path: "./env" });
+const env = require("dotenv").config();
 const server = express();
 const db = require("./db");
 
