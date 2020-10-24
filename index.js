93dcf040518e30e48c44b99633d41cceed5f5295
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-24 20:43:58 -0300

    teste

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ const env = require("dotenv").config();
 const server = express();
 const db = require("./db");
 
-mongoose
-  .connect(db.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log("the database is working");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(db.MONGODB_URI);
 
 server.get("/", function (req, res, next) {
   res.status(200).send("Hi, It works!");
