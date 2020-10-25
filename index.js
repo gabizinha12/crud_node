77818708605549820bea78176dc72dd7258d7db6
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-24 23:00:57 -0300

    habilitando cors

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ mongoose
 server.get("/", function (req, res, next) {
   res.status(200).send("Hi, It works!");
 });
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 server.use(cors());
 server.use(express.json());
