b6d8834b5982996add3cbe14939f0a458ce7cd1a
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-25 15:12:52 -0300

    cors is working

--- db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGODB_URI:
-    process.env.NODE_ENV === "production"
-      ? "mongodb+srv://deploy:deploy@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority"
-      : "mongodb+srv://softwrap:softwrap@cluster0.offec.mongodb.net/crud?retryWrites=true&w=majority",
-};
