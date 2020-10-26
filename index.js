07f15d3ef9c58ac5d77a16a34bc5be04ccc17c57
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-10-26 19:05:45 -0300

    cors

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ server.get('/', function (req, res, next) {
   res.status(200).send('Hi, It works!')
 })
 
+server.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 server.use(express.json())
 server.use(bodyParser.json())
 server.use('/api', require('./routes'))
